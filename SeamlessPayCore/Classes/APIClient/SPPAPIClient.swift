4173db2471b5a535c746d0f806b0929df6867c10
oleksandr-se <oleksandr.see@gmail.com>
2023-09-09 09:24:01 +0200

    SDKI-42 - WIP

--- SeamlessPayCore/Classes/APIClient/SPPAPIClient.swift
+++ SeamlessPayCore/Classes/APIClient/SPPAPIClient.swift
@@ -52,12 +52,190 @@ import Foundation
       billingAddress: billingAddress,
       name: name
     ) {
-      switch $0 {
-      case let .success(value):
-        success?(value)
-      case let .failure(error):
-        failure?(error.spError)
-      }
+      mapResult($0, success: success, failure: failure)
     }
+  }
+
+  @objc public func createCustomer(
+    name: String,
+    email: String,
+    address: SPAddress? = nil,
+    companyName: String? = nil,
+    notes: String? = nil,
+    phone: String? = nil,
+    website: String? = nil,
+    paymentMethods: [SPPaymentMethod]? = nil,
+    metadata: String? = nil,
+    success: ((SPCustomer) -> Void)?,
+    failure: ((SPError) -> Void)?
+  ) {
+    client.createCustomer(
+      name: name,
+      email: email,
+      address: address,
+      companyName: companyName,
+      notes: notes,
+      phone: phone,
+      website: website,
+      paymentMethods: paymentMethods,
+      metadata: metadata
+    ) {
+      mapResult($0, success: success, failure: failure)
+    }
+  }
+
+  @objc public func updateCustomer(
+    id: String,
+    name: String,
+    email: String,
+    address: SPAddress? = nil,
+    companyName: String? = nil,
+    notes: String? = nil,
+    phone: String? = nil,
+    website: String? = nil,
+    paymentMethods: [SPPaymentMethod]? = nil,
+    metadata: String? = nil,
+    success: ((SPCustomer) -> Void)?,
+    failure: ((SPError) -> Void)?
+  ) {
+    client.updateCustomer(
+      id: id,
+      name: name,
+      email: email,
+      address: address,
+      companyName: companyName,
+      notes: notes,
+      phone: phone,
+      website: website,
+      paymentMethods: paymentMethods
+    ) {
+      mapResult($0, success: success, failure: failure)
+    }
+  }
+
+  @objc public func retrieveCustomer(
+    id: String,
+    success: ((SPCustomer) -> Void)?,
+    failure: ((SPError) -> Void)?
+  ) {
+    client.retrieveCustomer(id: id) {
+      mapResult($0, success: success, failure: failure)
+    }
+  }
+
+  @objc public func createCharge(
+    token: String,
+    cvv: String? = nil,
+    capture: Bool,
+    currency: String? = nil,
+    amount: String? = nil,
+    taxAmount: String? = nil,
+    taxExempt: Bool,
+    tip: String? = nil,
+    surchargeFeeAmount: String? = nil,
+    description: String? = nil,
+    order: [String: String]? = nil,
+    orderId: String? = nil,
+    poNumber: String? = nil,
+    metadata: String? = nil,
+    descriptor: String? = nil,
+    entryType: String? = nil,
+    idempotencyKey: String? = nil,
+    success: ((SPCharge) -> Void)?,
+    failure: ((SPError) -> Void)?
+  ) {
+    client.createCharge(
+      token: token,
+      cvv: cvv,
+      capture: capture,
+      currency: currency,
+      amount: amount,
+      taxAmount: taxAmount,
+      taxExempt: taxExempt,
+      tip: tip,
+      surchargeFeeAmount: surchargeFeeAmount,
+      description: description,
+      order: order,
+      orderId: orderId,
+      poNumber: poNumber,
+      metadata: metadata,
+      descriptor: descriptor,
+      entryType: entryType,
+      idempotencyKey: idempotencyKey
+    ) {
+      mapResult($0, success: success, failure: failure)
+    }
+  }
+
+  @objc public func retrieveCharge(
+    id: String,
+    success: ((SPCharge) -> Void)?,
+    failure: ((SPError) -> Void)?
+  ) {
+    client.retrieveCharge(id: id) {
+      mapResult($0, success: success, failure: failure)
+    }
+  }
+
+  @objc public func listCharges(
+    success: (([String: Any]) -> Void)?,
+    failure: ((SPError) -> Void)?
+  ) {
+    client.listCharges {
+      mapResult($0, success: success, failure: failure)
+    }
+  }
+
+  @objc public func verify(
+    token: String,
+    cvv: String? = nil,
+    currency: String? = nil,
+    taxAmount: String? = nil,
+    taxExempt: Bool,
+    tip: String? = nil,
+    surchargeFeeAmount: String? = nil,
+    description: String? = nil,
+    order: [String: String]? = nil,
+    orderId: String? = nil,
+    poNumber: String? = nil,
+    metadata: String? = nil,
+    descriptor: String? = nil,
+    entryType: String? = nil,
+    idempotencyKey: String? = nil,
+    success: ((SPCharge) -> Void)?,
+    failure: ((SPError) -> Void)?
+  ) {
+    client.verify(
+      token: token,
+      cvv: cvv,
+      currency: currency,
+      taxAmount: taxAmount,
+      taxExempt: taxExempt,
+      tip: tip,
+      surchargeFeeAmount: surchargeFeeAmount,
+      description: description,
+      order: order,
+      orderId: orderId,
+      poNumber: poNumber,
+      metadata: metadata,
+      descriptor: descriptor,
+      entryType: entryType,
+      idempotencyKey: idempotencyKey
+    ) {
+      mapResult($0, success: success, failure: failure)
+    }
+  }
+}
+
+private func mapResult<Success>(
+  _ result: Result<Success, SeamlessPayError>,
+  success: ((Success) -> Void)?,
+  failure: ((SPError) -> Void)?
+) {
+  switch result {
+  case let .success(value):
+    success?(value)
+  case let .failure(error):
+    failure?(error.spError)
   }
 }
