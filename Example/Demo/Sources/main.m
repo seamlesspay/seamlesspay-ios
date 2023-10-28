753c9bb67f71764fe8728c8ff8888d5932dff237
Oleksandr Pavliuk <pavliuk.aleksandr@gmail.com>
2023-10-28 17:37:16 +0100

    feat: SDKI-41: wip

---
+++ Example/Demo/Sources/main.m
@@ -0,0 +1,18 @@
+/**
+ * Copyright (c) Seamless Payments, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#import "AppDelegate.h"
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+  NSString *appDelegateClassName;
+  @autoreleasepool {
+    // Setup code that might create autoreleased objects goes here.
+    appDelegateClassName = NSStringFromClass([AppDelegate class]);
+  }
+  return UIApplicationMain(argc, argv, nil, appDelegateClassName);
+}
