aac5d4c1c1027590a6a46d5052de3848f4fb336d
Oleksandr Pavliuk <pavliuk.aleksandr@gmail.com>
2023-10-31 21:40:40 +0000

    feat: SDKI-41: wip

---
+++ Example/PodsDemo/PodsDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PodsDemo
+//
+//  Created by macuser on 31.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
