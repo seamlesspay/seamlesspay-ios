a10f26399e1057664daa3a4ab288176059a30e41
oleksandr-se <oleksandr.see@gmail.com>
2024-01-29 19:47:49 +0000

    WIP

---
+++ SeamlessPayCore/Sources/UI/SPPaymentCardTextFieldTmp.swift
@@ -0,0 +1,17 @@
+// **
+// * Copyright (c) Seamless Payments, Inc.
+// *
+// * This source code is licensed under the MIT license found in the
+// * LICENSE file in the root directory of this source tree.
+// *
+
+import UIKit
+import Foundation
+
+@IBDesignable
+class SPPaymentCardTextFieldTmp: UIColor {
+
+  public var delegate: SPPaymentCardTextFieldDelegate
+
+  public var font: UIFont
+}
