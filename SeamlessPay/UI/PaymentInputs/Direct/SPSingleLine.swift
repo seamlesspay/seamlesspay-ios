b86ee758bf8d003e76ac17a87b543818cc0e3e3c
oleksandr-se <oleksandr.see@gmail.com>
2024-03-24 20:30:30 +0000

    SDKI-75 - wip

---
+++ SeamlessPay/UI/PaymentInputs/Direct/SPSingleLine.swift
@@ -0,0 +1,26 @@
+// **
+// * Copyright (c) Seamless Payments, Inc.
+// *
+// * This source code is licensed under the MIT license found in the
+// * LICENSE file in the root directory of this source tree.
+// *
+
+import UIKit
+
+public extension SPPaymentCardTextField {}
+
+@objcMembers public class SPSingleLine: UIView {
+
+  let textField = SPPaymentCardTextField()
+//  public override init(frame: CGRect) {
+//    super.init(frame: frame)
+//  }
+//
+//  public required init?(coder: NSCoder) {
+//    super.init(coder: coder)
+//  }
+//
+//  public init() {
+//    super.init(frame: CGRectZero)
+//  }
+}
