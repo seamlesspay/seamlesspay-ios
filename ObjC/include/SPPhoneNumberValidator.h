1b3a215a0be75b4560457f5529172d5b31d8eb60
seamless-pay-ios <seamlesspayios@gmail.com>
2024-07-11 13:20:48 +0100

    feat: SDKI-92: Review and clean up the entire public interface of the SeamlessPayObjcC submodule

--- ObjC/include/SPPhoneNumberValidator.h
+++ ObjC/include/SPPhoneNumberValidator.h
@@ -11,18 +11,9 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface SPPhoneNumberValidator : NSObject
 
-+ (BOOL)stringIsValidPartialPhoneNumber:(NSString *)string;
-+ (BOOL)stringIsValidPhoneNumber:(nullable NSString *)string;
-+ (BOOL)stringIsValidPartialPhoneNumber:(NSString *)string
-                         forCountryCode:(nullable NSString *)countryCode;
-+ (BOOL)stringIsValidPhoneNumber:(NSString *)string
-                  forCountryCode:(nullable NSString *)countryCode;
 + (NSString *)formattedSanitizedPhoneNumberForString:(NSString *)string;
 + (NSString *)formattedSanitizedPhoneNumberForString:(NSString *)string
                                       forCountryCode:(nullable NSString *)countryCode;
-+ (NSString *)formattedRedactedPhoneNumberForString:(NSString *)string;
-+ (NSString *)formattedRedactedPhoneNumberForString:(NSString *)string
-                                     forCountryCode:(nullable NSString *)countryCode;
 
 @end
 
