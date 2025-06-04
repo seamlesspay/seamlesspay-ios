e6dfb2b446f8eeda873c3940fdcab249befb757c
seamless-pay-ios <seamlesspayios@gmail.com>
2025-06-04 14:29:35 +0100

    feat [expose-sdk-version] : Add SeamlessPaySDK with version 2.6.0

---
+++ SeamlessPay/SeamlessPaySDK.swift
@@ -0,0 +1,10 @@
+// **
+// * Copyright (c) Seamless Payments, Inc.
+// *
+// * This source code is licensed under the MIT license found in the
+// * LICENSE file in the root directory of this source tree.
+// *
+
+public enum SeamlessPaySDK {
+  public static let version = "2.6.0"
+}
